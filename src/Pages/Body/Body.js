3630d9f102072e54e963da721f1c86bc6599dc46
Akarsh Barar <barar.akarsh8@gmail.com>
2021-10-01 00:32:00 +0530

    body

--- src/Pages/Body/Body.js
+++ src/Pages/Body/Body.js
@@ -2,8 +2,31 @@ import React from 'react'
 import './Body.css'
 function Body() {
     return (
-        <div>
-            <h3>Body</h3>
+        <div className="home">
+            <img className="homeImage" src="https://m.media-amazon.com/images/I/71FSVYGiPEL._SX3000_.jpg" alt="image"/>
+            <div className="homeProducts">
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                <div>Hii</div>
+                <div>Hello</div>
+                
+            </div>
         </div>
     )
 }
